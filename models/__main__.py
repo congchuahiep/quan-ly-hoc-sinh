5c5e12d4494e72dc4ef19ec6e4133fe596f6aa58
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2024-12-21 14:43:43 +0700

    xoa doan code khong can thiet

--- models/__main__.py
+++ models/__main__.py
@@ -1,8 +1,5 @@
 from app import app, db
 
-from sqlalchemy import Column, Enum, Integer, String, Float, ForeignKey, Date
-from sqlalchemy.orm import relationship
-
 from models.user import NguoiDung, NhanVien, GiaoVien, QuanTri
 from models.classroom import LopHoc, DayLop
 from models.student import HocSinh, HocSinhLop
